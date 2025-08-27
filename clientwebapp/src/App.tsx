77c1f2fe6759f8a4e43d249fd2b404d0e15e7b59
Ruben Aspeslag <ruben.aspeslag@student.howest.be>
2025-08-28 00:21:24 +0200

    Add home button

--- clientwebapp/src/App.tsx
+++ clientwebapp/src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { VacationsList } from './components/vacations';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { VacationDetail } from "./components/vacationDetail";
 
 function App() {
   
   return (
     <div className="App">
-    <header className="App-header">
-      <h1>Vacation desteny</h1>
-    </header>
-    <main>
-    <Router>
-      <Routes>
-        <Route path="/" element={<VacationsList />} />
-        <Route path="/vacations/:id" element={<VacationDetail />} />
-      </Routes>
-    </Router>
-    </main>
-  </div>
+      <Router>
+        <header className="App-header">
+          <nav className="App-nav">
+            <Link to="/" className="home-button">🏠 Home</Link>
+          </nav>
+          <h1>Vacation desteny</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<VacationsList />} />
+            <Route path="/vacations/:id" element={<VacationDetail />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   );
 }
 
